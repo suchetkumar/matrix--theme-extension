6d11d719416f6a294c7419f938a1066639ececd0
Suchet Kumar <sukumar9876@gmail.com>
2023-04-28 01:04:15 -0500

    Add leave review button and fix bugs

--- src/popup.js
+++ src/popup.js
@@ -63,7 +63,7 @@ const randomColor = () => {
   var randomColor = Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
   let choice = "#" + randomColor;
   document.getElementById('color-picker').value = choice;
-  chrome.storage.local.set({colorChoice: choice});
+  saveOptions()
   document.getElementById('popup').setAttribute('bgcolor', choice);
 }
 
